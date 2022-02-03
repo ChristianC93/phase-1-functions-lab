2d1be6b150b641b4195afdc30ef3313d477ac67b
Christian Castillo <castillochristian3@gmail.com>
2022-02-03 10:55:42 -0500

    commit tree e0b773707fe98f3b8da94287d159a3efa4fe1bfa

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    return Math.abs(street - 42);
+}
+
+function distanceFromHqInFeet(street){
+    return (distanceFromHqInBlocks(street) * 264);   
+}
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, end){
+   if (distanceTravelledInFeet(start, end) <= 400){
+       return 0;
+   } else if (distanceTravelledInFeet(start, end) > 400 && distanceTravelledInFeet(start, end) <= 2000){
+       return (distanceTravelledInFeet(start, end) - 400) * 0.02;
+   } else if (distanceTravelledInFeet(start, end) > 2000 && distanceTravelledInFeet(start, end)<= 2500){
+       return 25;
+   } else if (distanceTravelledInFeet(start, end) > 2500){
+       return 'cannot travel that far';
+   }    
+}
